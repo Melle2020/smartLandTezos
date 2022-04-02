10edcbb9192ddddf2311756d77c18cee9136f49c
Melle2020 <mellengami@gmail.com>
2022-04-03 01:40:01 +0200

    fix heroku in package json

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<app-name>'));
+app.use(express.static(__dirname + '/dist/landrick-angular'));
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname +
-        '/dist/<app-name>/index.html'));
+        '/dist/landrick-angular/index.html'));
 });
 app.listen(process.env.PORT || 8080);
